1946bfe27bcfe3b49c5d5a49f5ecab98c6d53df7
Acumes <295277302@qq.com>
2017-10-20 15:50:11 +0800

    dev:ddd

--- src/main/java/com/htf/conf/CorsConfig.java
+++ src/main/java/com/htf/conf/CorsConfig.java
@@ -5,16 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by PC-FENG on 2017/8/21.
  */
 @Configuration
+//public class CorsConfig implements Filter {
 public class CorsConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.addAllowedHeader("*");
+//        corsConfiguration.addAllowedHeader("Access-Control-Allow-Origin", "*");
         corsConfiguration.addAllowedMethod("*");
         return corsConfiguration;
     }
@@ -25,4 +32,27 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", buildConfig());
         return new CorsFilter(source);
     }
+
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        System.out.println("filter init...");
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//        String origin = (String) servletRequest.getRemoteHost()+":"+servletRequest.getRemotePort();
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Content-Type,uuid");
+//        response.setHeader("Access-Control-Allow-Credentials","true");
+//        response.setHeader("uuid","*");
+//        filterChain.doFilter(servletRequest, servletResponse);
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("destroy...");
+//    }
 }
